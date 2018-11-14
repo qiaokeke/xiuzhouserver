bb3b3b54a7dde01662a8da14fb33b4a912efd9a0
Your Name <you@example.com>
2018-11-14 15:46:10 +0800

    config

--- src/config/config.go
+++ src/config/config.go
@@ -1,48 +1,47 @@
 package config
 
 import (
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"log"
 	"runtime"
 	"strings"
-	"log"
 )
 
 var ConfigPath string
 
 type Config struct {
-	Port string
-	Cmds [][][]byte
-	MeterIds map[string] string
-	DataSource string
+	Port        string
+	Cmds        [][][]byte
+	MeterIds    map[string]string
+	DataSource  string
 	DataSource2 string
 }
 
 /**
-	读取配置文件
- */
-func ReadConfig()  Config{
+读取配置文件
+*/
+func ReadConfig() Config {
 	/**
 	判断操作系统类型
-	 */
+	*/
 	os := runtime.GOOS
-	path :=""
-	if strings.Compare(os,"windows")==0{
-		path = "C:\\work\\go\\xiuzhouserver\\src\\config\\"+ConfigPath
-	}else {
-		path = "/root/work/go/xiuzhouserver/src/config/"+ConfigPath
+	path := ""
+	if strings.Compare(os, "windows") == 0 {
+		path = "C:\\work\\go\\xiuzhouserver\\src\\config\\" + ConfigPath
+	} else {
+		//path = "/root/work/go/xiuzhouserver/src/config/"+ConfigPath
+		path = "/etc/xiuzhouserver/" + ConfigPath
 	}
 
-	file,err:=ioutil.ReadFile(path)
-	if err!=nil{
-		log.Println("config file err:",err)
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Println("config file err:", err)
 	}
 	config := Config{}
-	err2 := json.Unmarshal(file,&config)
-	if err2!=nil{
-		log.Println("json err:",err2)
+	err2 := json.Unmarshal(file, &config)
+	if err2 != nil {
+		log.Println("json err:", err2)
 	}
 	return config
 }
-
-
--- src/daosql/daosql.go
+++ src/daosql/daosql.go
@@ -1,38 +1,37 @@
 package daosql
 
 import (
-	"log"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-
-	"time"
 	"entity"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"time"
 )
 
-func InsertAllInfos(datasource string)  {
+func InsertAllInfos(datasource string) {
 	log.Println("Dao insert")
 	defer func() {
-		if x := recover();x!=nil{
+		if x := recover(); x != nil {
 			log.Println("insert,err,flag")
 			return
 		}
 	}()
 	sqlString := "INSERT INTO power_meter_record (" +
-			"p_code," +
-			"p_time," +
-			"p_zxygdn," +
-			"p_zxygdn_1," +
-			"p_zxygdn_2," +
-			"p_zxygdn_3," +
-			"p_zxygdn_4" +
-			")" +
-				"VALUES(" +
-					"?,?,?,?,?," +
-					"?,?" +
-				")"
-	db, err:= sql.Open("mysql", datasource)
-	if err!=nil{
-		log.Fatal("open err:",err)
+		"p_code," +
+		"p_time," +
+		"p_zxygdn," +
+		"p_zxygdn_1," +
+		"p_zxygdn_2," +
+		"p_zxygdn_3," +
+		"p_zxygdn_4" +
+		")" +
+		"VALUES(" +
+		"?,?,?,?,?," +
+		"?,?" +
+		")"
+	db, err := sql.Open("mysql", datasource)
+	if err != nil {
+		log.Fatal("open err:", err)
 		return
 	}
 	defer db.Close()
@@ -49,22 +48,21 @@ func InsertAllInfos(datasource string)  {
 	}
 	defer stmt.Close()
 
-	for k,v :=range entity.PowerInfoMap{
-		_,err :=stmt.Exec(v.PowerMeterId,
+	for k, v := range entity.PowerInfoMap {
+		_, err := stmt.Exec(v.PowerMeterId,
 			time.Now().Format("20060102150405"),
-				v.Zxygdn,
-					v.Zxygdn1,
-						v.Zxygdn2,
-							v.Zxygdn3,
-								v.Zxygdn4)
+			v.Zxygdn,
+			v.Zxygdn1,
+			v.Zxygdn2,
+			v.Zxygdn3,
+			v.Zxygdn4)
 		if err != nil {
 			log.Fatal(err)
 		}
-		delete(entity.PowerInfoMap,k)
+		delete(entity.PowerInfoMap, k)
 	}
 	err = tx.Commit()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
